eea6c15b0a36fef3be398ec563ef6ecc7100a595
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-03-02 17:05:49 +0300

    add Main.java and MainTest.java

---
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void isEven() {
+
+        assertEquals(Main.isEven(1), false);
+    }
+}
